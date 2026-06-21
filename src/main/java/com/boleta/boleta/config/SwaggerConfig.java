14e718fd5539bc092ac05be4009f3b596c7b5ef2
NicasioBayron <bayrongd932@gmail.com>
2026-06-20 20:35:04 -0400

    Se agrega Swagger, pendiente de pruebas

--- src/main/java/com/boleta/boleta/config/SwaggerConfig.java
+++ src/main/java/com/boleta/boleta/config/SwaggerConfig.java
@@ -1,5 +1,19 @@
 package com.boleta.boleta.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API 2026 Provedores")
+                        .version("1.0")
+                        .description("Documentación de la API del microservicio de Provedores"));
+    }
 }
